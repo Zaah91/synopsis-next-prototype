67e430e1e05ed5ec58f92f39417257d52f55bfb8
Martin Rohde <zaah91@gmail.com>
2025-05-26 15:31:18 +0200

    Updates

---
+++ src/app/subpage1/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Subpage1() {
+  return (
+    <div className="min-h-screen p-8 pb-20 sm:p-20">
+      <header className="text-center mb-8">
+        <h1 className="text-4xl font-bold">Welcome to subpage1</h1>
+        <p className="text-lg mt-2">In the 'Next.js App Router' prototype</p>
+      </header>
+    </div>
+  );
+}
---
+++ src/app/subpage2/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Subpage2() {
+  return (
+    <div className="min-h-screen p-8 pb-20 sm:p-20">
+      <header className="text-center mb-8">
+        <h1 className="text-4xl font-bold">Welcome to subpage2</h1>
+        <p className="text-lg mt-2">In the 'Next.js App Router' prototype</p>
+      </header>
+    </div>
+  );
+}
