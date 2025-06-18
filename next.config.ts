733fbe0d946795acbcbd46e7c802642d01d2e1e1
Martin Rohde <zaah91@gmail.com>
2025-06-18 13:17:12 +0200

    New deployment try

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+  /* config options here */
 };
 
 export default nextConfig;
