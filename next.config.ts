829f0de13c9bf5a6da718be91a63c67c4af9c420
Martin Rohde <zaah91@gmail.com>
2025-06-18 14:48:06 +0200

    changes to config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  base: process.env.BASE_PATH || "/synopsis-next-prototype",
 };
 
 export default nextConfig;
