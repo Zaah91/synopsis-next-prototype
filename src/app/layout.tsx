f68cc99efb7d1717340750702dad58deccf37f2c
Martin Rohde <zaah91@gmail.com>
2025-06-18 12:03:03 +0200

    Edits to layout and new metadata file. metadataCollection added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,8 @@
 import "./globals.css";
-import { Metadata } from "next";
 import Navbar from "@/components/Navbar";
+import siteMetadata from "./metadataCollection";
 
-export const metadata: Metadata = {
-  title: {
-    template: "%s | Next.js Prototype",
-    default: "Next.js Prototype",
-  },
-  description: "An example of using metadata in Next.js with robots.txt and sitemap.xml",
-  metadataBase: new URL("https://github.com/Zaah91/synopsis-next-prototype"),
-  openGraph: { // Open Graph metadata for social media sharing
-    title: "Next.js Prototype",
-    description: "An example of using metadata in Next.js with robots.txt and sitemap.xml",
-    url: "https://github.com/Zaah91/synopsis-next-prototype",
-    siteName: "Next.js Prototype",
-    images: [
-      {
-        url: "https://github.com/Zaah91/synopsis-next-prototype/blob/master/public/sitemap-snippet.PNG",
-        width: 1200,
-        height: 630,
-        alt: "Open Graph Image",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-}
+export const metadata = siteMetadata;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
---
+++ src/app/metadataCollection.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+
+const siteMetadata: Metadata = {
+  title: {
+    template: "%s | Next.js Prototype",
+    default: "Next.js Prototype",
+  },
+  description: "An example of using metadata in Next.js with robots.txt and sitemap.xml",
+  metadataBase: new URL("https://github.com/Zaah91/synopsis-next-prototype"),
+  openGraph: { // Open Graph metadata for social media sharing
+    title: "Next.js Prototype",
+    description: "An example of using metadata in Next.js with robots.txt and sitemap.xml",
+    url: "https://github.com/Zaah91/synopsis-next-prototype",
+    siteName: "Next.js Prototype",
+    images: [
+      {
+        url: "https://github.com/Zaah91/synopsis-next-prototype/blob/master/public/duck.webp?raw=true",
+        width: 1200,
+        height: 630,
+        alt: "Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+}
+
+export default siteMetadata;
